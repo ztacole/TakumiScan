4e1a70b6f109e525bf1bd7c5198b0b74cb87c7ab
Jeki <ahmadzaqi490@gmail.com>
2024-12-18 14:52:43 +0700

    little little

--- app/src/main/java/com/zetta/takumiscan/presentation/main/menu/home/notes/NotesDetailActivity.kt
+++ app/src/main/java/com/zetta/takumiscan/presentation/main/menu/home/notes/NotesDetailActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.zetta.takumiscan.R
+import com.zetta.takumiscan.databinding.ActivityNotesDetailBinding
 
 class NotesDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNotesDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_notes_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityNotesDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.statusBarColor = getColor(R.color.navy)
     }
 }
