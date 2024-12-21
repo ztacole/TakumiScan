4555cb5ac47e9c10dcfc4900a5dd8b0218181846
Jeki <ahmadzaqi490@gmail.com>
2024-12-21 19:03:05 +0700

    add function showdialog

--- app/src/main/java/com/zetta/takumiscan/util/core/CoreFunction.kt
+++ app/src/main/java/com/zetta/takumiscan/util/core/CoreFunction.kt
@@ -1,6 +1,8 @@
 package com.zetta.takumiscan.util.core
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.util.TypedValue
 
 object CoreFunction {
@@ -9,4 +11,26 @@ object CoreFunction {
         dp.toFloat(),
         resources.displayMetrics
     ).toInt()
+
+    fun Context.showDialog(
+        title: String,
+        message: String,
+        positiveButtonText: String,
+        onPositiveButtonClick: DialogInterface.OnClickListener,
+        negativeButtonText: String? = null,
+        onNegativeButtonClick: DialogInterface.OnClickListener? = null,
+        cancellable: Boolean = true
+    ){
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setCancelable(cancellable)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButtonText, onPositiveButtonClick)
+
+        negativeButtonText?.let { text ->
+            dialogBuilder.setNegativeButton(text, onNegativeButtonClick)
+        }
+
+        dialogBuilder.show()
+    }
 }
