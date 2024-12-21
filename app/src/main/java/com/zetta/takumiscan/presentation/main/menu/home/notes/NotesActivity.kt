78d5ff17f30005aef1f9feb89c15ebe586621262
Jeki <ahmadzaqi490@gmail.com>
2024-12-21 19:02:27 +0700

    add swipe event on recyclerview

--- app/src/main/java/com/zetta/takumiscan/presentation/main/menu/home/notes/NotesActivity.kt
+++ app/src/main/java/com/zetta/takumiscan/presentation/main/menu/home/notes/NotesActivity.kt
@@ -1,17 +1,20 @@
 package com.zetta.takumiscan.presentation.main.menu.home.notes
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zetta.takumiscan.R
 import com.zetta.takumiscan.data.local.DBHelper
 import com.zetta.takumiscan.databinding.ActivityNotesBinding
+import com.zetta.takumiscan.util.CacheController
 import com.zetta.takumiscan.util.MarginItemDecoration
 import com.zetta.takumiscan.util.core.CoreFunction.dpToPx
+import com.zetta.takumiscan.util.core.CoreFunction.showDialog
 
 class NotesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotesBinding
@@ -25,6 +28,7 @@ class NotesActivity : AppCompatActivity() {
         window.statusBarColor = getColor(R.color.navy)
 
         setRecyclerView()
+        loadData()
 
         binding.btnBack.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
@@ -37,14 +41,58 @@ class NotesActivity : AppCompatActivity() {
         }
     }
 
+    private fun loadData(){
+        val listNotes = dbHelper.getListNotes()
+        val adapter = NotesAdapter(listNotes)
+        binding.rvNotes.adapter = adapter
+
+        val touchHelper = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(ItemTouchHelper.ACTION_STATE_IDLE, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun getSwipeDirs(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                return if (viewHolder.adapterPosition != 0){
+                    super.getSwipeDirs(recyclerView, viewHolder)
+                }
+                else ItemTouchHelper.ACTION_STATE_IDLE
+            }
+
+            override fun onSwiped(
+                viewHolder: RecyclerView.ViewHolder,
+                direction: Int
+            ) {
+                val note = listNotes[viewHolder.adapterPosition - 1]
+                if (direction == ItemTouchHelper.LEFT){
+                    Toast.makeText(this@NotesActivity, "Deleted ${viewHolder.adapterPosition}, id ${note.id}", Toast.LENGTH_SHORT).show()
+                    adapter.notifyItemRemoved(viewHolder.adapterPosition)
+                }
+                else{
+                    Toast.makeText(this@NotesActivity, "Edited", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        touchHelper.attachToRecyclerView(binding.rvNotes)
+    }
+
     private fun setRecyclerView(){
         binding.rvNotes.layoutManager = LinearLayoutManager(this)
 
         val bottomMargin = dpToPx(0)
         val lastItemBottomMargin = dpToPx(96)
         binding.rvNotes.addItemDecoration(MarginItemDecoration(bottomMargin, lastItemBottomMargin))
+    }
 
-        val listNotes = dbHelper.getListNotes()
-        binding.rvNotes.adapter = NotesAdapter(listNotes)
+    override fun onResume() {
+        super.onResume()
+        loadData()
     }
 }
