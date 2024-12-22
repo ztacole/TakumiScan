c84967cd292d72d03ae3d7ea0fc3128861041f9b
Jeki <ahmadzaqi490@gmail.com>
2024-12-22 14:58:09 +0700

    remove OnTriggeredGeofenceListener

--- app/src/main/java/com/zetta/takumiscan/presentation/main/MainActivity.kt
+++ app/src/main/java/com/zetta/takumiscan/presentation/main/MainActivity.kt
@@ -19,7 +19,7 @@ import com.zetta.takumiscan.presentation.main.menu.history.HistoryFragment
 import com.zetta.takumiscan.presentation.main.menu.home.HomeFragment
 import com.zetta.takumiscan.util.geofence.OnGeofenceTriggeredListener
 
-class MainActivity : AppCompatActivity(), OnGeofenceTriggeredListener {
+class MainActivity : AppCompatActivity(){
     lateinit var binding: ActivityMainBinding
     private lateinit var dbHelper: DBHelper
     private lateinit var histories: List<History>
@@ -101,20 +101,5 @@ class MainActivity : AppCompatActivity(), OnGeofenceTriggeredListener {
                 }
             }
         })
-    }
-
-    override fun onGeofenceEnter() {
-        binding.btnQR.setOnClickListener {
-            Intent(this, ScanActivity::class.java).also {
-//                it.putExtra("lokasi", "${userLocation.latitude}, ${userLocation.longitude}")
-                startActivity(it)
-            }
-        }
-    }
-
-    override fun onGeofenceExit() {
-        binding.btnQR.setOnClickListener {
-            Toast.makeText(this, "Fitur ini hanya aktif jika kamu berada di kawasan SMKN 24 Jakarta", Toast.LENGTH_SHORT).show()
-        }
     }
 }
