41fda6f2ee55b67a8effa9ca61a3f6e7934941a9
Jeki <ahmadzaqi490@gmail.com>
2025-01-12 01:11:23 +0700

    kirim data ke server

---
+++ app/src/main/java/com/zetta/takumiscan/data/remote/APIController.kt
@@ -0,0 +1,49 @@
+package com.zetta.takumiscan.data.remote
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataOutputStream
+import java.io.OutputStream
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+
+class APIController(url: String, method: String) {
+    companion object{
+        const val BASE_URL = "https://backend24.site/Rian/XI/takumi/"
+    }
+
+    private var conn: HttpURLConnection = URL(BASE_URL+url).openConnection() as HttpURLConnection
+
+    init {
+        conn.requestMethod = method
+        conn.doOutput = true
+    }
+
+    fun execute(postData: Map<String, Any>? = null, onResponse: (String)-> Unit) {
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                postData?.let { data ->
+                    val postDataString = data.entries.joinToString("&") { "${it.key}=${it.value}" }
+                    conn.outputStream.use { output ->
+                        OutputStreamWriter(output).use { writer ->
+                            writer.write(postDataString)
+                            writer.flush()
+                        }
+                    }
+                }
+                val response = conn.inputStream.bufferedReader().use { it.readText() }
+                withContext(Dispatchers.Main) {
+                    onResponse(response)
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    onResponse("Error: ${e.message}")
+                }
+            }
+        }
+    }
+}
